f7793a430f3345535dc44da1775b1fa2399a4b8f
Arne Kleinschmidt <arne.kleinschmidt@uxi.de>
2018-05-08 15:42:27 +0200

    updates test expectations, since svg-to-jsx now allows xmlns tags

--- test/svg-to-jsx-loader-spec.js
+++ test/svg-to-jsx-loader-spec.js
@@ -4,7 +4,7 @@ const expected = [
   `import React from 'react';\n` +
 
   `function ForwardIcon(props) {` +
-  `  return (<svg height="24" width="24" version="1.1" viewBox="0 0 24 24" {...props}>` +
+  `  return (<svg height="24" width="24" version="1.1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>` +
   `\n\t<path d="M21 11l-7-7v4C7 9 4 14 3 19c2.5-3.5 6-5.1 11-5.1V18l7-7z"/>\n` +
   `</svg>);` +
   `}\n` +
@@ -15,7 +15,7 @@ const expected = [
   `import React from 'react';\n` +
 
   `function FRwardIcN(props) {` +
-  `  return (<svg height="24" width="24" version="1.1" viewBox="0 0 24 24" {...props}>` +
+  `  return (<svg height="24" width="24" version="1.1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>` +
   `\n\t<path d="M21 11l-7-7v4C7 9 4 14 3 19c2.5-3.5 6-5.1 11-5.1V18l7-7z"/>\n` +
   `</svg>);` +
   `}\n` +
@@ -26,7 +26,7 @@ const expected = [
   `import React from 'react';\n` +
 
   `function ForwardIcon(props) {` +
-  `  return (<svg height="24" width="24" version="1.1" viewBox="0 0 24 24" {...props}>` +
+  `  return (<svg height="24" width="24" version="1.1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>` +
   `\n\t<path d="M21 11l-7-7v4C7 9 4 14 3 19c2.5-3.5 6-5.1 11-5.1V18l7-7z"/>\n` +
   `</svg>);` +
   `}\n` +
@@ -48,7 +48,7 @@ const expected = [
 module.exports = (actual) => {
   console.assert(
     expected.find(x => x === actual),
-    `svg-to-jsx-loader should return stateless functional react component` +
+    `svg-icon-to-react-component-loader should return stateless functional react component` +
     `\n\n==> Actual result:\n${actual}` +
     `\n\n==> Expected result:\n${expected}`
   );
